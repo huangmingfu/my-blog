a3d13d2b8f22d7964f4441a0d990e0db856abe93
13242839219 <212149997@qq.com>
2024-03-09 15:59:26 +0800

    feat

---
+++ docs/.vitepress/blog-theme.ts
@@ -0,0 +1,122 @@
+// 主题独有配置
+import { getThemeConfig } from '@sugarat/theme/node'
+
+import {getDiffDate} from '../../utils/tool';
+// 开启RSS支持（RSS配置）
+// import type { Theme } from '@sugarat/theme'
+
+// const baseUrl = 'https://sugarat.top'
+// const RSS: Theme.RSSOptions = {
+//   title: '粥里有勺糖',
+//   baseUrl,
+//   copyright: 'Copyright (c) 2018-present, 粥里有勺糖',
+//   description: '你的指尖,拥有改变世界的力量（大前端相关技术分享）',
+//   language: 'zh-cn',
+//   image: 'https://img.cdn.sugarat.top/mdImg/MTY3NDk5NTE2NzAzMA==674995167030',
+//   favicon: 'https://sugarat.top/favicon.ico',
+// }
+
+// 所有配置项，详见文档: https://theme.sugarat.top/
+const blogTheme = getThemeConfig({
+  // 开启RSS支持
+  // RSS,
+
+  // 搜索
+  // 默认开启pagefind离线的全文搜索支持（如使用其它的可以设置为false）
+  // 如果npx pagefind 时间过长，可以手动将其安装为项目依赖 pnpm add pagefind
+  // search: false,
+
+  // 页脚
+  footer:  [{
+    version: false,
+    icpRecord: {
+      name: `Copyrights © 2024-${new Date().getFullYear()} 丨 a无名`,
+      link: '',
+      icon:false
+    },
+  },
+  {
+    version: false,
+    icpRecord: {
+      name: `基于VitePress+@sugarat/theme主题`,
+      link: '',
+      icon:false
+    },
+  },
+  {
+    version: false,
+    icpRecord: {
+      name: `已运行${getDiffDate('2024-03-08')}天`,
+      link: '',
+      icon:false
+    },
+  }],
+
+  // 主题色修改
+  themeColor: 'el-blue',
+
+  // 文章默认作者
+  author: 'a无名',
+
+  hotArticle: {
+    title: '🔥 精选文章',
+    nextText: '换一组',
+    pageSize: 9,
+    empty: '暂无精选内容'
+  },
+
+  // 友链
+  // friend: [
+  //   {
+  //     nickname: '粥里有勺糖',
+  //     des: '你的指尖用于改变世界的力量',
+  //     avatar:
+  //       'https://img.cdn.sugarat.top/mdImg/MTY3NDk5NTE2NzAzMA==674995167030',
+  //     url: 'https://sugarat.top',
+  //   },
+  //   {
+  //     nickname: 'Vitepress',
+  //     des: 'Vite & Vue Powered Static Site Generator',
+  //     avatar:
+  //       'https://vitepress.dev/vitepress-logo-large.webp',
+  //     url: 'https://vitepress.dev/',
+  //   },
+  // ],
+
+  // 公告
+  // popover: {
+  //   title: '公告',
+  //   body: [
+  //     { type: 'text', content: '👇公众号👇---👇 微信 👇' },
+  //     {
+  //       type: 'image',
+  //       src: 'https://img.cdn.sugarat.top/mdImg/MTYxNTAxODc2NTIxMA==615018765210'
+  //     },
+  //     {
+  //       type: 'text',
+  //       content: '欢迎大家加群&私信交流'
+  //     },
+  //     {
+  //       type: 'text',
+  //       content: '文章首/文尾有群二维码',
+  //       style: 'padding-top:0'
+  //     },
+  //     {
+  //       type: 'button',
+  //       content: '作者博客',
+  //       link: 'https://sugarat.top'
+  //     },
+  //     {
+  //       type: 'button',
+  //       content: '加群交流',
+  //       props: {
+  //         type: 'success'
+  //       },
+  //       link: 'https://theme.sugarat.top/group.html',
+  //     }
+  //   ],
+  //   duration: 0
+  // },
+})
+
+export { blogTheme }
---
+++ docs/resources/Icon.md
@@ -0,0 +1,19 @@
+---
+tag:
+ - Resources
+description: 个人收集的一些即拿即用的图标网站~
+---
+# 🥰 在线icon图标/emoji资源网站
+
+## icon/svg图标
+[国旗SVG图标](https://nucleoapp.com/svg-flag-icons)  
+[技术相关 SVG 图标汇总](https://svgl.app/)  
+[Yesicon - 开源、免费的矢量图标库 ](https://yesicon.app/)  
+[Iconbuddy](https://iconbuddy.app/)  
+[xicons](https://www.xicons.org/#/zh-CN)    
+[Iconbolt](https://www.iconbolt.com/)   
+[icones](https://icones.js.org)   
+[Iconify图标](https://icon-sets.iconify.design/)   
+
+## emoji
+[Emoji大全](https://www.emojiall.com/zh-hans/all-emojis?type=normal) - 可直接复制粘贴到md文件
