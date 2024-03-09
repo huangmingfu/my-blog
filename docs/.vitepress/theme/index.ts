f6d14dd180de01e367064f15db2908ede6484673
13242839219 <212149997@qq.com>
2024-03-09 22:20:49 +0800

    gitee 自动推送

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -5,9 +5,10 @@ import BlogTheme from '@sugarat/theme'
 
 // 自定义主题色
 // import './user-theme.css'
+
 import { inBrowser } from 'vitepress'
 import busuanzi from 'busuanzi.pure.js'
-import { useLive2d } from 'vitepress-theme-website'
+// import { useLive2d } from 'vitepress-theme-website'
 
 export default {
     extends: BlogTheme,
@@ -19,29 +20,29 @@ export default {
             }
         }
     },
-    setup() {
-        if (inBrowser) {
-            //看板娘
-            useLive2d({
-                enable: true,
-                model: {
-                    url: 'https://raw.githubusercontent.com/iCharlesZ/vscode-live2d-models/master/model-library/girls-frontline/HK416-2/normal/model.json'
-                },
-                display: {
-                    position: 'left',
-                    width: '250px',
-                    height: '400px',
-                    xOffset: '0px',
-                    yOffset: '0px'
-                },
-                mobile: {
-                    show: true
-                },
-                react: {
-                    opacity: 0.8
-                }
-            })
-        }
-    }
+    // setup() {
+    //     if (inBrowser) {
+    //         //看板娘
+    //         useLive2d({
+    //             enable: true,
+    //             model: {
+    //                 url: 'https://raw.githubusercontent.com/iCharlesZ/vscode-live2d-models/master/model-library/girls-frontline/HK416-2/normal/model.json'
+    //             },
+    //             display: {
+    //                 position: 'left',
+    //                 width: '250px',
+    //                 height: '400px',
+    //                 xOffset: '0px',
+    //                 yOffset: '0px'
+    //             },
+    //             mobile: {
+    //                 show: true
+    //             },
+    //             react: {
+    //                 opacity: 0.8
+    //             }
+    //         })
+    //     }
+    // }
 }
 // export default BlogTheme
